fdbbc84aa6f8e6ab43912e9a5ca18a3ddf40a8c9
YangHgRi <yanghgri@gmail.com>
2025-09-18 18:48:41 +0800

    feat(algo): 新增 ListNode 定义

--- 21.合并两个有序链表.java
+++ 21.合并两个有序链表.java
@@ -5,16 +5,6 @@
  */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         // Base case: if either list is null, return the other list
