134fb38313b9257eb95ad6d4baef39cfbdff3e72
YangHgRi <yanghgri@gmail.com>
2025-09-20 19:02:06 +0800

    refactor(algo): optimize variable declaration in addTwoNumbers

--- 2.add-two-numbers.go
+++ 2.add-two-numbers.go
@@ -24,16 +24,18 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	carry := 0               // 进位
 	// 循环直到两个链表都为空且没有进位
 	for l1 != nil || l2 != nil || carry != 0 {
-		val1 := 0
+		val1, val2 := 0, 0
+
 		if l1 != nil {
 			val1 = l1.Val
 			l1 = l1.Next
 		}
-		val2 := 0
+
 		if l2 != nil {
 			val2 = l2.Val
 			l2 = l2.Next
 		}
+
 		sum := val1 + val2 + carry
 		carry = sum / 10 // 计算新的进位
 
